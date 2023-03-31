53d209179175b8c3df90192bafff92e85186ae3b
Oscar Lopez <olop1597@gmail.com>
2023-03-30 21:44:33 -0700

    Connecting the components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+//Import data to make sure all components are connected
+import NavBar from './components/NavBar';
+import Gallery from './components/gallery';
+import Post from './components/post';
+import Footer from './components/footer';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavBar/>
+      <Gallery/>
+      <Footer/>
     </div>
   );
 }
