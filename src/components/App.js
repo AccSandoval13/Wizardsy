20dfad6fbfe301b931580a8d758e8604a21bbfa5
Oscar Lopez <olop1597@gmail.com>
2023-04-01 11:16:45 -0700

    Trying this again

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+//Import data to make sure all components are connected
+import NavBar from './NavBar';
+import Gallery from './gallery';
+import Post from './post';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <NavBar/>
+      <Gallery/>
+     
+      <div className="App">
+        {}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/gallery.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Post from './post'
+const wizardsyPost = require('./content')
+
+const Gallery = () => {
+    return (
+        <div>
+            {/* Post Layout i.e. (data, i) => etc... */}
+        </div>
+    )
+}
+export default Gallery
