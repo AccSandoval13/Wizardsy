10ac0cd6ad8f145e2b95f3e95ea85e48d590e8de
Hire Privy <eddierivas@Eddies-MBP.home>
2023-04-05 20:43:00 -0700

    adding routes

--- controllers/places.js
+++ controllers/places.js
@@ -4,8 +4,6 @@ const MongoClient = require('mongodb').MongoClient;
 const url = 'mongodb+srv://UserWiz:UserPassword@wizcluster.23xxeul.mongodb.net';
 const User = require ('./models/user'); 
 
-const router = express.Router()
-
 
 // Importing Database and Collection From MongoDB 
 const usersDB = 'users'; 
@@ -49,53 +47,4 @@ MongoClient.connect(url)
       });
   })
 
- 
-//Routes
-
-//Home
   
-  router.get('/', (req, res) => {
-    res.render('/', { home })
-          })
-          .catch(err => {
-              console.log('err', err)
-              res.render('error404')
-          })
-//About
-  
-          router.get('/about', (req, res) => {
-            res.render('/about', { about })
-                  })
-                  .catch(err => {
-                      console.log('err', err)
-                      res.render('error404')
-                  })
-//Today
-                  router.get('/today', (req, res) => {
-                    res.render('/today', { today })
-                          })
-                          .catch(err => {
-                              console.log('err', err)
-                              res.render('error404')
-                          })
-//Create
-router.get('/create', (req, res) => {
-  res.render('/create', { create })
-        })
-        .catch(err => {
-            console.log('err', err)
-            res.render('error404')
-        })
-
-        module.exports = router;
-
-      // Routes 
-
-    router.get ('/', function (req, res){
-      res.render('/home', { home })
-    })
-    .catch(err => {
-      res.render('error404')
-  })
-
-  module.exports = feed;
